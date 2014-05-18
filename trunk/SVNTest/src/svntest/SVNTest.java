413895fca94c5e747f6fdcfebb42a79d1bc99454
toluco2580@gmail.com <toluco2580@gmail.com@4120b2c6-eca8-2864-6bcd-9494018f1609>
2014-05-18 07:00:37 +0000

    test1

---
+++ trunk/SVNTest/src/svntest/SVNTest.java
@@ -0,0 +1,12 @@
+package svntest;
+
+public class SVNTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+    
+}
