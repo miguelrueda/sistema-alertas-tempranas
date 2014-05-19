01c2cea86b5baa2f617707f9524fbaf86ca0b90e
toluco2580@gmail.com <toluco2580@gmail.com@4120b2c6-eca8-2864-6bcd-9494018f1609>
2014-05-19 14:27:59 +0000

    Another change test

--- SAXParserTest/src/saxparsertest/Test2.java
+++ SAXParserTest/src/saxparsertest/Test2.java
@@ -3,7 +3,6 @@ package saxparsertest;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,8 +12,9 @@ public class Test2 {
     public static void main(String[] args) throws MalformedURLException, IOException {
         System.out.println("Read file from WEB");
         File f = new File("http://cve.mitre.org/data/downloads/allitems-cvrf-year-2014.xml");
+        //Una linea de ejemplo
         if (f.canRead()) {
-            System.out.println("Esperando lecturs");
+            System.out.println("Esperando lectura");
         } else {
             System.out.println("Cannot read");
         }
