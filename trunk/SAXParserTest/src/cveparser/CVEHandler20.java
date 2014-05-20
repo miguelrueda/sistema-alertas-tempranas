e38c7340f1c7b3347ead85fa466334d2fefa6b21
toluco2580@gmail.com <toluco2580@gmail.com@4120b2c6-eca8-2864-6bcd-9494018f1609>
2014-05-20 17:28:02 +0000

    Parser2.0 changes

--- trunk/SAXParserTest/src/cveparser/CVEHandler20.java
+++ trunk/SAXParserTest/src/cveparser/CVEHandler20.java
@@ -1,6 +1,12 @@
 package cveparser;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -9,6 +15,7 @@ public class CVEHandler20 extends DefaultHandler {
 
     private List<CVE> cveList = null;
     private CVE nuevoCVE;
+    private boolean bPublished = false;
 
     public List<CVE> getCveList() {
         return cveList;
@@ -16,17 +23,55 @@ public class CVEHandler20 extends DefaultHandler {
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        System.out.println("ABRE: " + qName);
+        //System.out.println("ABRE: " + qName);
+        if (qName.equalsIgnoreCase("ENTRY")) {
+            String name = attributes.getValue("id");
+            nuevoCVE = new CVE(name);
+            if (null == cveList) {
+                cveList = new ArrayList<>();
+            }
+        } else if (qName.equalsIgnoreCase("vuln:published-datetime")) {
+            bPublished = true;
+        }
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
-        System.out.println("CIERRA: " + qName);
+        //System.out.println("CIERRA: " + qName);
+        if (qName.equalsIgnoreCase("ENTRY")) {
+            nuevoCVE.setCVSS_score("-1");
+            nuevoCVE.setDescription("NON");
+            nuevoCVE.setModified(new Date());
+            nuevoCVE.setReferences(new ArrayList<CVEReference>());
+            nuevoCVE.setSeverity("NON");
+            nuevoCVE.setVuln_soft(new ArrayList<VulnSoftware>());
+            cveList.add(nuevoCVE);
+        }
     }
 
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
-        super.characters(ch, start, length); //To change body of generated methods, choose Tools | Templates.
+        if (bPublished) {
+            try {
+                //2014-01-31T18:55:04.503-05:00
+                String published_date = new String(ch, start, length);
+                String [] spl_pub = published_date.split("T");
+                String date = spl_pub[0];
+                //System.out.println(date);
+                String time = spl_pub[1];
+                //System.out.println(time);
+                String [] spl_time = time.split("\\.");
+                String res_time = spl_time[0];
+                //System.out.println(res_time);
+                String datetime = date + " " + res_time;
+                
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                nuevoCVE.setPublished(formatter.parse(datetime));
+                bPublished = false;
+            } catch (ParseException ex) {
+                Logger.getLogger(CVEHandler20.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
 }
