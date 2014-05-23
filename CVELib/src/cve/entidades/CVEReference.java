69b66371a59467ac0a824b71c96c64c985110ae1
toluco2580@gmail.com <toluco2580@gmail.com@4120b2c6-eca8-2864-6bcd-9494018f1609>
2014-05-23 15:06:46 +0000

    Biblioteca CVE

---
+++ CVELib/src/cve/entidades/CVEReference.java
@@ -0,0 +1,76 @@
+package cve.entidades;
+
+/**
+ * Clase que representa la entidad referencia sobre un CVE
+ *
+ * @author t41507
+ * @version 20.05.2014
+ */
+public class CVEReference {
+
+    /**
+     * Atributos
+     * 
+     */
+    private String source;
+    private String url;
+
+    /**
+     * Constructor
+     */
+    public CVEReference() {
+    }
+
+    /**
+     * Constructor
+     *
+     * @param url dirección de la referencia
+     * @param source fuente de la referencia
+     */
+    public CVEReference(String url, String source) {
+        this.url = url;
+        this.source = source;
+    }
+
+    /**
+     * GETTER
+     *
+     * @return fuente de la referencia
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     * SETTER
+     *
+     * @param source fuente de la referencia
+     */
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    /**
+     * GETTER
+     *
+     * @return url de la referencia
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * SETTER
+     *
+     * @param url url de la referencia
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "CVEReference{" + "source=" + source + ", url=" + url + '}';
+    }
+
+}
