2a9201caee80c52a144abc0b3a7b147693d21d3c
toluco2580@gmail.com <toluco2580@gmail.com@4120b2c6-eca8-2864-6bcd-9494018f1609>
2014-06-03 17:55:13 +0000

    Cambios - Modulo de Vulnerabilidades

--- trunk/SistemaAlertasBM/src/java/jsf/filters/LoginFilter.java
+++ trunk/SistemaAlertasBM/src/java/jsf/filters/LoginFilter.java
@@ -9,10 +9,12 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import jsf.mgbns.LoginBean;
 
+@WebFilter(filterName = "LoginFilter", urlPatterns = {"/secured/*"})
 public class LoginFilter implements Filter {
 
     private static final Logger LOG = Logger.getLogger(LoginFilter.class.getName());
@@ -25,20 +27,38 @@ public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         //Obtener el loginBean a partir de la sesion
-        LoginBean loginBean = (LoginBean) ((HttpServletRequest) request).getSession().getAttribute("loginBean");
+        HttpServletRequest req = (HttpServletRequest) request;
+        LoginBean loginBean = (LoginBean) req.getSession().getAttribute("loginBean");
         LOG.log(Level.SEVERE, "Obteniendo referencia a login");
         /*
          Para la primer solicitud de la aplicacion no hay loginBean en la sesion, entonces el cliente necesita loggearse
          Para otras solicitudes loginBean esta presente pero necesitamos chcar si el usuario se ha logeado
          */
-        if (loginBean == null || !loginBean.isLoggedIn()) {
-            LOG.log(Level.SEVERE, "EL usuario no esta autenticado.");
-            String contextPath = ((HttpServletRequest) request).getContextPath();
-            LOG.log(Level.SEVERE, "Contexto: {0}", contextPath);
-            ((HttpServletResponse) response).sendRedirect(contextPath + "/Login.xhtml");
-        } else {
-            LOG.log(Level.SEVERE, "Usuario Autenticado - OK/Cambiando al recurso solicitado");
-            chain.doFilter(request, response);
+        /*
+         if (loginBean == null || !loginBean.isLoggedIn()) {
+         LOG.log(Level.SEVERE, "EL usuario no esta autenticado.");
+         String contextPath = ((HttpServletRequest) request).getContextPath();
+         LOG.log(Level.SEVERE, "Contexto: {0}", contextPath);
+         ((HttpServletResponse) response).sendRedirect(contextPath + "/Login.xhtml");
+         } else {
+         LOG.log(Level.SEVERE, "Usuario Autenticado - OK/Cambiando al recurso solicitado");
+         chain.doFilter(request, response);
+         }*/
+        try {
+            if (loginBean != null && loginBean.isLoggedIn()) {
+                //El usuario esta logeado --> Completar la solicitud
+                LOG.log(Level.INFO, "Usuario Autenticado -> Completando Solicitud.");
+                chain.doFilter(request, response);
+            } else {
+                HttpServletResponse resp = (HttpServletResponse) response;
+                LOG.log(Level.INFO, "Usuario No Autenticado -> Redireccionando al inicio ");
+                LOG.log(Level.INFO, "Contexto: {0}", req.getContextPath());
+                req.getSession().invalidate();
+                resp.sendRedirect(req.getContextPath() + "/Login.xhtml");
+
+            }
+        } catch (IOException | ServletException e) {
+            LOG.log(Level.INFO, "Problema -->" + e.getMessage());
         }
     }
 
