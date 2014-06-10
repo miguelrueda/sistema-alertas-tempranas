1605c701602edffe05c0d3cd04d9ba51d6b90d80
toluco2580@gmail.com <toluco2580@gmail.com@4120b2c6-eca8-2864-6bcd-9494018f1609>
2014-06-10 18:54:02 +0000

    VulnUpdates

--- SistemaAlertasBM/src/java/jsf/converters/ProductListConverter.java
+++ SistemaAlertasBM/src/java/jsf/converters/ProductListConverter.java
@@ -5,29 +5,43 @@ import java.util.logging.Logger;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import jpa.entities.ListaProducto;
 
 /**
  *
  * @author t41507
  */
+@FacesConverter(value = "productListConverter")
 public class ProductListConverter implements Converter {
 
     private static final Logger LOG = Logger.getLogger(ProductListConverter.class.getName());
 
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        LOG.log(Level.INFO, "FacesContext: {0}", context);
-        LOG.log(Level.INFO, "UIComponent: {0}", component);
-        LOG.log(Level.INFO, "VALUE: {0}", value);
-        return "VALUERETURNED";
+        LOG.log(Level.INFO, "ASOB - Llega: {0}", value);
+        if (value.trim().equals("")) {
+            return "TEMP"; 
+        } else {
+            //return new ListaProducto(Integer.parseInt(value));
+            return new ListaProducto(1, value);
+            //return "TEMP";
+        }
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
-        LOG.log(Level.INFO, "FacesContext: {0}", context);
-        LOG.log(Level.INFO, "UIComponent: {0}", component);
-        LOG.log(Level.INFO, "VALUE: {0}", value);
-        return "VALUERETURNED";
+        LOG.log(Level.INFO, "ASST - LLega: {0}", value.toString());
+        if (value == null || value.equals("")) {
+            return "TEMP";
+        } else {
+            LOG.log(Level.INFO, "RECIBO --> {0}", value);
+            LOG.log(Level.INFO, "String creado");
+            //return String.valueOf(new ListaProducto(Integer.parseInt(value.toString())).getNombre());
+            //return ((ListaProducto) vale).getNombre();
+            ListaProducto nueva = new ListaProducto(Integer.parseInt(value.toString()), "Lista " + value);
+            return nueva.getNombre();
+        }
     }
 
 }
