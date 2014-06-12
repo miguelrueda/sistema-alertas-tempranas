6dae81bf3eb24442e0d3069c07b58d4251a86e36
toluco2580@gmail.com <toluco2580@gmail.com@4120b2c6-eca8-2864-6bcd-9494018f1609>
2014-06-12 03:18:55 +0000

    Ejemplo JSP Servlet AJX

---
+++ MyAjaxApp/src/java/com/ajax/Composer.java
@@ -0,0 +1,52 @@
+package com.ajax;
+
+public class Composer {
+
+    String id;
+    String firstName;
+    String lastName;
+    String category;
+
+    public Composer() {
+    }
+
+    public Composer(String id, String firstName, String lastName, String category) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.category = category;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
