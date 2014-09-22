e925146fd40a37ee412a5920922758a4c6515810
toluco2580@gmail.com <toluco2580@gmail.com@4120b2c6-eca8-2864-6bcd-9494018f1609>
2014-09-22 19:52:41 +0000

    Agregadas consultas de información de vulnerabilidades y estadisticas en dao para la funcionalidad del dashboard

--- trunk/SISALBMv2/test/org/banxico/ds/sisal/prueba/RecentsTest.java
+++ trunk/SISALBMv2/test/org/banxico/ds/sisal/prueba/RecentsTest.java
@@ -1,17 +1,47 @@
 package org.banxico.ds.sisal.prueba;
 
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.TreeMap;
 import org.banxico.ds.sisal.dao.VulnerabilityDAO;
-import org.banxico.ds.sisal.entities.Vulnerabilidad;
-import java.util.List;
 
 public class RecentsTest {
-    
+
     public static void main(String[] args) {
         VulnerabilityDAO vdao = new VulnerabilityDAO();
-        List<Vulnerabilidad> recientes = vdao.obtenerVulnerabilidadesRecientes();
-        for (Vulnerabilidad vuln : recientes) {
-            System.out.println(vuln);
+        /*
+         List<Vulnerabilidad> recientes = vdao.obtenerVulnerabilidadesRecientes();
+         for (Vulnerabilidad vuln : recientes) {
+         System.out.println(vuln);
+         }
+         */
+        int res = 0;
+        HashMap<String, Integer> datos = vdao.obtenerEstadisticasFabricantes();
+        System.out.println(datos);
+        HashMap<String, Integer> estadistica = new HashMap<String, Integer>();
+        Iterator<String> iterator = datos.keySet().iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            if (datos.get(key) > 50) {
+                estadistica.put(key, datos.get(key));
+            }
         }
+        System.out.println(estadistica);
+        TreeMap sorted = new TreeMap(estadistica);
+        String json = "";
+        Gson gson = new Gson();
+        json = gson.toJson(sorted);
+        System.out.println("json = " + json);
+        System.out.println("Resultado: " + res);
+        
+        /*
+         Iterator<String> keySetIterator = datos.keySet().iterator();
+         while (keySetIterator.hasNext()) {
+         String llave = keySetIterator.next();
+         System.out.println("{llave:" + llave + ",valor:" + datos.get(llave) + "}");
+         }
+         */
     }
-    
+
 }
